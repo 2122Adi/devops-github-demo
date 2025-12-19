9076be35262eeabb84b27d4725e279c504804d8c
2122adi <9004562056aditya@gmail.com>
2025-12-19 15:50:26 +0530

    test4

---
+++ app.py
@@ -0,0 +1,1 @@
+print("Hello DevOps App is running...")
--- test.py
+++
@@ -1,5 +0,0 @@
-for count in range(6):
-   print ("Iteration no. {}".format(count))
-else:
-   print ("for loop over. Now in else block")
-print ("End of for loop")
